b32318dffc013819fc21a2c979c1caddf030e18e
Zachary Kuchar <zacharyk323@live.com>
2023-07-31 13:09:38 -0400

    Added Title Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Baner from "./components/Baner";
+import Title from "./components/Title";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       {/* <h1>Weather banner/ Maybe use pokemnon??</h1> */}
       <Baner />
       <h1>Title header with name and description</h1>
+      <Title />
       <h1>Description of types of work that I have done</h1>
       <h1>Work experience descriptions</h1>
       <h1>Top projects</h1>
---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Title() {
+  return (
+    <div className='title'>
+      <h1>Zachary Edward Kuchar</h1>
+      
+        <p>Description of myself</p>
+      
+    </div>
+  )
+}
+
+export default Title
