45aeda6170d0265cdd7a2821341f022bf3a3a53f
Zachary Kuchar <zacharyk323@live.com>
2023-07-31 10:49:49 -0400

    Initial wireframe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather banner/ Maybe use pokemnon??</h1>
+      <h1>Title header with name and description</h1>
+      <h1>Description of types of work that I have done</h1>
+      <h1>Top projects</h1>
+      <h3>Add links to github repos</h3>
+      <h1>In progress projects</h1>
+      <h3>Add link to pokemon project repo</h3>
+      <h1>The footer/ Maybe with other socials</h1>
     </div>
   );
 }
