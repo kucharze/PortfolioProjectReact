b79326dee2ca2979f07cd150f2340a66651f21b2
Zachary Kuchar <zacharyk323@live.com>
2023-07-31 11:59:19 -0400

    Linked banner, updated wireframe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Baner from "./components/Baner";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather banner/ Maybe use pokemnon??</h1>
+      <Baner />
       <h1>Title header with name and description</h1>
       <h1>Description of types of work that I have done</h1>
+      <h1>Work experience descriptions</h1>
       <h1>Top projects</h1>
       <h3>Add links to github repos</h3>
       <h1>In progress projects</h1>
