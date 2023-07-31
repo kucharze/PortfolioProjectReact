1e3a01e7e0d4719533d03422a91926efd29db05b
Zachary Kuchar <zacharyk323@live.com>
2023-07-31 14:23:12 -0400

    Added top projects page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import logo from "./logo.svg";
 import "./App.css";
 import Baner from "./components/Baner";
 import Title from "./components/Title";
+import WorkDesc from "./components/WorkDesc";
+import TopProjects from "./components/TopProjects";
 
 function App() {
   return (
     <div className="App">
       {/* <h1>Weather banner/ Maybe use pokemnon??</h1> */}
       <Baner />
-      <h1>Title header with name and description</h1>
+      {/* <h1>Title header with name and description</h1> */}
       <Title />
-      <h1>Description of types of work that I have done</h1>
-      <h1>Work experience descriptions</h1>
-      <h1>Top projects</h1>
-      <h3>Add links to github repos</h3>
+      {/* <h1>Description of types of work that I have done</h1> */}
+      <WorkDesc />
+
+      {/* <h1>Top projects</h1> */}
+      <TopProjects />
+
       <h1>In progress projects</h1>
       <h3>Add link to pokemon project repo</h3>
       <h1>The footer/ Maybe with other socials</h1>
---
+++ src/components/TopProjects.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TopProjects() {
+  return (
+    <div className='top'>
+      <h1>Top Projects</h1>
+      <h3>Loop through different link urls</h3>
+      <h3>Add links to github repos</h3>
+    </div>
+  )
+}
+
+export default TopProjects
