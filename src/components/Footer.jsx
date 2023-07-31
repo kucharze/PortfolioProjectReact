8d3e9840e569ff06adf80fdc2fff022489047251
Zachary Kuchar <zacharyk323@live.com>
2023-07-31 14:37:40 -0400

    Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <h1>The footer</h1>
+      <h1>Include socials info</h1>
+    </div>
+  )
+}
+
+export default Footer
