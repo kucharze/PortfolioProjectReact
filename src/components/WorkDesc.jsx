294984735e9a04abe8708f3876a9f1607c58f24e
Zachary Kuchar <zacharyk323@live.com>
2023-07-31 14:28:08 -0400

    Added work description

---
+++ src/components/WorkDesc.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function WorkDesc() {
+  return (
+    <div className='work'>
+      <h1>Work Descriptions</h1>
+      <h3>Work experience descriptions</h3>
+      <h4>Map out a list (pull in from models folder?)</h4>
+    </div>
+  )
+}
+
+export default WorkDesc
