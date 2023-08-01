bd3ecea8a226526fc2718e078b2947dfd0f18313
Zachary Kuchar <zacharyk323@live.com>
2023-08-01 09:45:44 -0400

    Added image adjusted other elements

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,8 +4,11 @@ function Title() {
   return (
     <div className='title'>
       <h1>Zachary Edward Kuchar</h1>
+      <img src="https://www.pngitem.com/pimgs/m/78-786293_1240-x-1240-0-avatar-profile-icon-png.png" 
+      alt=""
+      className='profile' />
       <div className='description'>
-        <p>A young software developer looking for a job.  
+        <p>A young software developer with a passion for programming.  
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
             Expedita dolor totam laboriosam illo iure dolore qui maxime. 
             Distinctio voluptatem nemo sit minima hic quam mollitia.</p>
