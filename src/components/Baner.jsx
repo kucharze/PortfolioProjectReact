410de95f0718217ae4554a00c2c38072752551fd
Zachary Kuchar <zacharyk323@live.com>
2023-08-01 15:22:11 -0400

    Banner winner and health logic

--- src/components/Baner.jsx
+++ src/components/Baner.jsx
@@ -3,17 +3,21 @@ import { AppContext } from '../contexts/context';
 
 
 function Baner() {
-  let {pokemon,setPokemon, opp,setOpp, getPokemon, getOpp, moves, oppMoves, getMove,vis} = useContext(AppContext)
+  let {pokemon,setPokemon, opp,setOpp,health,oppHealth, 
+    getPokemon, getOpp, moves, oppMoves, getMove,vis,doMove,
+  win, winner} = useContext(AppContext)
 
   useEffect(() => {
-    //getPok();
     getPokemon();
-    
     getOpp();
   }, []);
+
   return (
     <div className='banner'>
       <h3>Pokemon battle of the day</h3>
+      {
+        win && (winner == "player" ? <h1>You win</h1> : <h1>You loose</h1>)
+      }
       <div className="battle">
         {
           pokemon ? <div className='pokemondisplay'>
@@ -21,6 +25,7 @@ function Baner() {
               <div className="info">
                   <p>Height: {pokemon.height} M.</p>
                   <p>Weight: {pokemon.weight} G.</p>
+                  <p>Health: {health}</p>
               </div>
               <img src={pokemon.sprites.back_default} alt="" className='pokemon' />
               <br/>
@@ -29,8 +34,9 @@ function Baner() {
               }
               
               {
-                moves && moves.map((item)=>{
-                  return <button className='moves' key={item.name}>{item.name}</button>
+                moves && moves.map((item,i)=>{
+                  return <button className='moves' key={item.name} 
+                  onClick={()=>{doMove(i)}} disabled={win}>{item.name}</button>
                 })
               }
           </div>
@@ -42,12 +48,13 @@ function Baner() {
               <div className="info">
                   <p>Height: {opp.height} M.</p>
                   <p>Weight: {opp.weight} G.</p>
+                  <p>Health: {oppHealth}</p>
               </div>
               <img src={opp.sprites.front_default} alt="" className='pokemon' />
               <br/>
               <p className='oppmoves'>
               {
-                moves && moves.map((item)=>{
+                oppMoves && oppMoves.map((item)=>{
                   return item.name + "  "
                 })
               }
