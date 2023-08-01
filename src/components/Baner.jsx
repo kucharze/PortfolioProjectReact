1573bb4cbc88814c2afdf444a9a450713bee82bf
Zachary Kuchar <zacharyk323@live.com>
2023-08-01 10:07:03 -0400

    Added section for opponent

--- src/components/Baner.jsx
+++ src/components/Baner.jsx
@@ -3,41 +3,41 @@ import { AppContext } from '../contexts/context';
 
 
 function Baner() {
-  let { opp} = useContext(AppContext)
-    const [pokemon,setPokemon] = useState(null)
-
-  const getPok = async () => {
-    try {
-      let item = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${Math.floor(
-          Math.random() * (300 - 1 + 1) + 1
-        )}`
-      );
-      let data = await item.json();
-      //console.log("opp:", data);
-      setPokemon(data);
-    } catch (err) {
-      console.log("We did not find a valid pokemon");
-    }
-  };
+  let {pokemon,setPokemon, opp,setOpp, getPokemon, getOpp} = useContext(AppContext)
 
   useEffect(() => {
-    getPok();
+    //getPok();
+    //getPokemon();
+    //getOpp();
   }, []);
   return (
     <div className='banner'>
       <h3>Pokemon of the day</h3>
-      {
-        pokemon ? <div className='pokemondisplay'>
-            <h4>Name: {pokemon.name}</h4>
-            <div className="info">
-                <p>Height: {pokemon.height} M.</p>
-                <p>Weight: {pokemon.weight} G.</p>
-            </div>
-            <img src={pokemon.sprites.front_default} alt="" className='pokemon' />
-        </div>
-        :<h1>Loading</h1>
-      }
+      <div className="battle">
+        {
+          pokemon ? <div className='pokemondisplay'>
+              <h4>Name: {pokemon.name}</h4>
+              <div className="info">
+                  <p>Height: {pokemon.height} M.</p>
+                  <p>Weight: {pokemon.weight} G.</p>
+              </div>
+              <img src={pokemon.sprites.back_default} alt="" className='pokemon' />
+          </div>
+          :<h1>Loading</h1>
+        }
+        {
+          opp ? <div className='pokemondisplay'>
+              <h4>Name: {opp.name}</h4>
+              <div className="info">
+                  <p>Height: {opp.height} M.</p>
+                  <p>Weight: {opp.weight} G.</p>
+              </div>
+              <img src={opp.sprites.front_default} alt="" className='pokemon' />
+          </div>
+          :<h1>Loading</h1>
+        }
+      </div>
+      
     </div>
   )
 }
