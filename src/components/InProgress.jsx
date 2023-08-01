173a7940cd9f3f8630bc94f22c0e7708125f365e
Zachary Kuchar <zacharyk323@live.com>
2023-08-01 09:49:26 -0400

    Removed unneeded tag

--- src/components/InProgress.jsx
+++ src/components/InProgress.jsx
@@ -5,7 +5,6 @@ function InProgress({project}) {
   return (
     <div className='progress'>
       <h1>In Progress projects</h1>
-      <h3>Bring in Link to pokemon project</h3>
       <div className="progLinks">
         {
             project.map((item)=>{
