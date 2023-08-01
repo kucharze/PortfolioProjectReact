17d3341cf3a9caf69d0dec83450d36884c4e60fe
Zachary Kuchar <zacharyk323@live.com>
2023-07-31 20:44:11 -0400

    Added new project link

--- src/models/projects.js
+++ src/models/projects.js
@@ -7,6 +7,10 @@ const projects = [
     name: "React employee list",
     url: "https://github.com/kucharze/EmployeeList",
   },
+  {
+    name: "Per Scholas Crazy eights game",
+    url: "https://github.com/kucharze/PerScholasProject1",
+  },
 ];
 
 module.exports = { projects };
