14a1156f7038cbaded2c2f27544e8a94bfff2090
Zachary Kuchar <zacharyk323@live.com>
2023-07-31 21:14:19 -0400

    Adjusted project styles

--- src/components/TopProjects.jsx
+++ src/components/TopProjects.jsx
@@ -5,13 +5,15 @@ function TopProjects({projects}) {
   return (
     <div className='top'>
       <h1>Top Projects</h1>
-      {
-        projects.map((item)=>{
-            return <div>
-                <Link className='githubLink' key={item.name} to={item.url}>{item.name}</Link>
-            </div>
-        })
-      }
+      <div className="projLinks">
+        {
+            projects.map((item)=>{
+                return <Link className='githubLink' key={item.name} to={item.url}>{item.name}</Link>
+                
+            })
+        }
+      </div>
+      
     </div>
   )
 }
