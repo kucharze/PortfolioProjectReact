fd08de41fe78592d4f9c0c88c82c1d4220edd847
Zachary Kuchar <zacharyk323@live.com>
2023-08-01 11:09:04 -0400

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='footer'>
-      <h3>Email Make a link: zacharyk323@live.com</h3>
+      <h3>Email: zacharyk323@live.com</h3>
       <h3>Phone: 412-469-2233</h3>
     </div>
   )
