97b03777c88392cb7e81dbeb46d870bb3dec60ad
Zachary Kuchar <zacharyk323@live.com>
2023-08-03 13:16:52 -0400

    Added skill component

---
+++ src/components/Skill.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Skill() {
+  return (
+    <div className='skill'>
+      <h1>The skill</h1>
+      <h1>bar</h1>
+      <div className='bar'></div>
+    </div>
+  )
+}
+
+export default Skill
