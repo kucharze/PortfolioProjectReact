51f5ff4ce0fffd98efbce02c52f5da10d3a073d7
Zachary Kuchar <zacharyk323@live.com>
2023-08-03 15:03:32 -0400

    Updated work experience graph

--- src/components/WorkDesc.jsx
+++ src/components/WorkDesc.jsx
@@ -4,16 +4,13 @@ import Skill from './Skill'
 function WorkDesc() {
   return (
     <div className='work'>
-      <h1>Work Descriptions</h1>
-      <h3>Work experience descriptions</h3>
-      <h4>PUT GRAPH IN HERE WITH SKILLS Map out a list (pull in from models folder?)</h4>
       <div className="graph">
-        <h1>Hello</h1>
-        <Skill level={4}/>
-        <Skill/>
-        <Skill/>
-        <Skill/>
-        <Skill/>
+        <h1>Experience level</h1>
+        <Skill skill={'Java'} level={4}/>
+        <Skill skill={"Javascript"} level={4}/>
+        <Skill skill={'C#'} level={3}/>
+        <Skill skill={'python'} level={3}/>
+        <Skill skill={'C++'} level={3}/>
       </div>
     </div>
   )
