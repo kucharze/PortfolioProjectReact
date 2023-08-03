7d622f5d8b008dab6236f98f1b46fa25c8393a45
Zachary Kuchar <zacharyk323@live.com>
2023-08-03 15:50:22 -0400

    Tweaked levels for skills

--- src/components/WorkDesc.jsx
+++ src/components/WorkDesc.jsx
@@ -6,14 +6,26 @@ function WorkDesc() {
     <div className='work'>
       <div className="graph">
         <h1>Experience level</h1>
-        <Skill skill={'Java'} level={4}/>
-        <Skill skill={"Javascript"} level={4}/>
-        <Skill skill={'C#'} level={3}/>
-        <Skill skill={'python'} level={3}/>
-        <Skill skill={'C++'} level={3}/>
+        <Skill skill={'Java'} level={8}/>
+        <Skill skill={"Javascript"} level={8}/>
+        <Skill skill={'C#'} level={7}/>
+        <Skill skill={'Python'} level={7}/>
+        <Skill skill={'C++'} level={6}/>
       </div>
     </div>
   )
 }
 
 export default WorkDesc
+
+//Google
+/*
+Familiar with Pilars of object oriented programing
+Understand why these are the most value principles
+
+Select a hackerrank problem and copy the problem
+DO NOT ATTEMPT IT!!!!
+
+Get one that you feel you can do, and one that seems impossible
+
+*/
